4520909dd424b5ae825c7dcdb2246d45d84e4b09
unknown <Tadeo@Tadeo-Komputer.(none)>
2013-05-29 18:21:32 +0200

    ClientManager jako singleton.

--- connection.js
+++ connection.js
@@ -1,4 +1,5 @@
 var net = require('net');
+var events = require('events');
 
 exports.ClientSocket = function () {
 	net.Socket.call(this);
@@ -9,12 +10,50 @@ exports.ClientSocket = function () {
 	});
 	this.on('error', function(e) {
 		console.log(e.code);
+		clientManager.freeSocket(this);
 		eventEmitter.emit('reconnect');
 	});
 	this.on('end', function() {
+		clientManager.freeSocket(this);
 		eventEmitter.emit('reconnect');
 	});
 };
 
 exports.ClientSocket.prototype = new net.Socket();
 exports.ClientSocket.prototype.constructor = exports.ClientSocket;
+
+exports.ClientManager = function(maxConnections) {
+	var freeSockets = new Array(maxConnections);
+	var occupiedSockets = [];
+	
+	for (var i = 0;i<maxConnections;i++)
+	{
+		freeSockets[i] = new exports.ClientSocket();
+	}
+	
+	this.connect = function(port,address)
+	{
+		if (freeSockets.length > 0)
+		{
+			var socket = freeSockets[Math.floor(Math.random()*freeSockets.length)];
+			socket.connect(port,address, function() {
+				occupySocket(socket); 
+			});
+		}
+		else
+		{
+			console.log('No free sockets.');
+		}
+	}
+	
+	this.occupySocket = function(socket) {
+		occupiedSockets.push(freeSockets.splice(freeSockets.indexOf(socket),1));
+	}
+	
+	this.freeSocket = function(socket) {
+		freeSockets.push(occupiedSockets.splice(occupiedSockets.indexOf(socket),1));
+	}
+}
+
+//Singleton
+exports.clientManager = new ClientManager();
